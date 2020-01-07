8e6080391fa76976e1ce8e5dc631ec23fe02edbf
destinyu <1191641830@qq.com>
2020-01-07 23:27:25 +0800

    01-02第一天的基础练习

---
+++ day_01_test.py
@@ -0,0 +1,21 @@
+name="Eric"
+fname=" marc "
+#句子的连接
+print("Hello "+name+",would you like to learn some Python today")
+#大小写
+print(name.upper())
+print(name.lower())
+print(name.title())#首字母大写
+#使用转义字符输出“
+print("Albert Einstein once said,\"A person who never make a mistake ...\"")#转义字符输出双引号
+print('\n')
+print(fname)
+#去掉前空格
+print(fname.lstrip())
+#去掉后空格
+print(fname.rstrip())
+#去掉前后空格
+print(fname.strip())
+print(0.1+0.1)
+names=['aa','bb','cc','dd']
+print(names[1])
