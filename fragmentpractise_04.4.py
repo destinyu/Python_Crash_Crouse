6421c67d93902ba32eff9d6a898aab5edd7fd9bb
destinyu <1191641830@qq.com>
2020-01-11 17:38:00 +0800

    04.练习

---
+++ fragmentpractise_04.4.py
@@ -0,0 +1,8 @@
+plist=['apple','banana','peach','tomato','potato']
+print("the first three items in the list are:")
+print(plist[0:3])
+print("the middle three items in the list are:")
+print(plist[1:4])
+print("the last three items in the list are:")
+print(plist[-3:])
+print(plist[2:])
