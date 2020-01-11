e20dc54d19dcd5d28c763b07514d523b052b011b
destinyu <1191641830@qq.com>
2020-01-11 17:38:37 +0800

    04.元组的使用（不能修改赋值）

---
+++ dimensions_04.5.py
@@ -0,0 +1,11 @@
+dimensions=(200,50)
+print(dimensions[0])
+for dimension in dimensions:
+    print(dimension)
+#五种简单的食品，放入一个元组：
+foods=('apple','banana','peach','potato','tomato')
+for food in foods:
+    print(food)
+foods=('a','b','c','d','e')
+for food in foods:
+    print(food)
