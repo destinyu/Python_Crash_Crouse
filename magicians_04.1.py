6a6d3f6940483cddc8caba62a63f6ea3f36b0edd
destinyu <1191641830@qq.com>
2020-01-11 17:23:13 +0800

    04.列表及for循环使用（注意缩进）

---
+++ magicians_04.1.py
@@ -0,0 +1,7 @@
+magicians=['alice','david','carolina']
+for magician in magicians:
+    print(magician)
+
+for magician in magicians:
+    print(magician.title()+",i like your magic")
+print("thanks for the show")
