b93b3730128df31b08cc0570b415d64dae99fffd
destinyu <1191641830@qq.com>
2020-01-31 17:55:17 +0800

    10.json用法

---
+++ store_10.py
@@ -0,0 +1,18 @@
+import json
+
+def greet_user():
+    filename='username.json'
+    try:
+        with open(filename) as f_obj:
+            username=json.load(f_obj)
+    except FileNotFoundError:
+        username=input("What is your name?")
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print("we will remeber you when you come back," + username + " !")
+    else:
+        print("Welcome back! "+username+" !")
+
+
+
+
